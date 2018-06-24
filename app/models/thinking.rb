43686503a7f6e48e95a7614267ce0b27c2dea57b
TakuyaIwai <tha1225.1010@gmail.com>
2018-06-24 07:16:21 +0000

    指摘部分修正

--- app/models/thinking.rb
+++ app/models/thinking.rb
@@ -1,5 +1,6 @@
 class Thinking < ApplicationRecord
   validates :factor, presence: true
+  validates :action_plan1, presence: true
   has_many :relationships, dependent: :destroy
   default_scope -> { order(created_at: :desc) }
 end
